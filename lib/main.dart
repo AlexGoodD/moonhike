978a21a02471a8d93521d1881fffb061688a50ae
Mauricio Gallegos <maugal.cst@gmail.com>
2024-11-12 11:13:01 -0600

    logout y bug fix completado

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(), // Ruta para la página de login
+        '/map': (context) => MapScreen(),   // Ruta para la pantalla principal
+      },
       home: FutureBuilder(
         future: FirebaseAuth.instance.authStateChanges().first,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator()); // Cargando
+            return Center(child: CircularProgressIndicator()); // Pantalla de carga
           } else if (snapshot.hasData) {
-            return MapScreen(); // Usuario autenticado, va a la pantalla principal
+            return MapScreen(); // Usuario autenticado, redirige a la pantalla principal
           } else {
-            return LoginPage(); // No autenticado, va a la pantalla de login
+            return LoginPage(); // Usuario no autenticado, redirige a la pantalla de login
           }
         },
       ),
