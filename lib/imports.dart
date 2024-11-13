997e889aac24ba133756d52e74a0f054702e90f4
Mauricio Gallegos <maugal.cst@gmail.com>
2024-11-12 22:05:07 -0600

    carpetas de profile_screen creadas, pubsepc.yaml librerias

--- lib/imports.dart
+++ lib/imports.dart
@@ -38,7 +38,7 @@ export 'package:moonhike/domain/use_cases/route_risk_calculator.dart';
 export 'package:moonhike/presentation/screens/map_screen.dart';
 export 'package:moonhike/presentation/screens/login_screen.dart';
 export 'package:moonhike/presentation/screens/map_controller.dart';
-export 'package:moonhike/presentation/screens/profile_screen.dart';
+export 'package:moonhike/presentation/screens/profile_screens/profile_screen.dart';
 export 'package:moonhike/presentation/screens/register.dart';
 export 'package:moonhike/presentation/widgets/floating_action_buttons.dart';
 export 'package:moonhike/presentation/widgets/map_widget.dart';
