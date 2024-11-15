edd737e3e6620315db17ee50d14cd7ab449b12a2
Mauricio Gallegos <maugal.cst@gmail.com>
2024-11-15 16:52:34 -0600

    edicion de creador de reportes, mejora diseño de botón de crear reporte

--- lib/presentation/widgets/floating_action_buttons.dart
+++ lib/presentation/widgets/floating_action_buttons.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:moonhike/imports.dart';
 
 class FloatingActionButtons extends StatelessWidget {
   final Future<void> Function() onStartRoute;
@@ -17,13 +17,29 @@ class FloatingActionButtons extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         SizedBox(height: 10),
-        FloatingActionButton(
-          onPressed: () async {
-            await onCreateReport();
-          },
-          child: Icon(Icons.report),
-          backgroundColor: Colors.red,
-          heroTag: 'createReport',
+        Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(12),
+            gradient: LinearGradient(
+              colors: [const Color.fromARGB(255, 86, 86, 174), ProfileColors.backgroundBottom],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: FloatingActionButton(
+            onPressed: () async {
+              await onCreateReport();
+            },
+            child: Icon(
+              Boxicons.bx_map_pin,
+              size: 36.0, // Tamaño del icono
+              color: Colors.white, // Color del icono
+            ),
+            backgroundColor: Colors.transparent, // Hace que el fondo del FAB sea transparente
+            elevation: 0, // Quita la sombra si no es necesaria
+            heroTag: 'createReport',
+          ),
         ),
       ],
     );
--- lib/presentation/widgets/report_dialog.dart
+++ lib/presentation/widgets/report_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moonhike/imports.dart';
 
 class ReportDialog extends StatefulWidget {
   final Function(String, String) onReportTypeSelected;
@@ -11,47 +12,118 @@ class ReportDialog extends StatefulWidget {
 
 class _ReportDialogState extends State<ReportDialog> {
   String note = ""; // Almacena la nota escrita por el usuario
+  String? selectedReportType; // Almacena el tipo de reporte seleccionado
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('Selecciona el tipo de reporte'),
-      content: SingleChildScrollView( // Agregado para evitar overflow
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              leading: Icon(Icons.lightbulb),
-              title: Text('Mala iluminación'),
-              onTap: () => widget.onReportTypeSelected('Mala iluminación', note),
-            ),
-            ListTile(
-              leading: Icon(Icons.warning),
-              title: Text('Inseguridad'),
-              onTap: () => widget.onReportTypeSelected('Inseguridad', note),
-            ),
-            ListTile(
-              leading: Icon(Icons.directions_walk),
-              title: Text('Interés peatonal'),
-              onTap: () => widget.onReportTypeSelected('Poca vialidad peatonal', note),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Nota (opcional)'),
-              onChanged: (value) {
-                setState(() {
-                  note = value;
-                });
-              },
-            ),
-          ],
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [SettingsColor.backgroundTop, SettingsColor.backgroundBottom],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Selecciona el tipo de reporte',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 10),
+              RadioListTile<String>(
+                value: 'Mala iluminación',
+                groupValue: selectedReportType,
+                title: Text('Mala iluminación', style: TextStyle(color: Colors.white)),
+                secondary: Icon(Boxicons.bx_bulb, color: const Color.fromARGB(255, 255, 165, 82)),
+                onChanged: (value) {
+                  setState(() {
+                    selectedReportType = value;
+                  });
+                },
+              ),
+              RadioListTile<String>(
+                value: 'Inseguridad',
+                groupValue: selectedReportType,
+                title: Text('Inseguridad', style: TextStyle(color: Colors.white)),
+                secondary: Icon(Boxicons.bx_dislike, color: const Color.fromARGB(255, 108, 92, 255)),
+                onChanged: (value) {
+                  setState(() {
+                    selectedReportType = value;
+                  });
+                },
+              ),
+              RadioListTile<String>(
+                value: 'Interés peatonal',
+                groupValue: selectedReportType,
+                title: Text('Interés peatonal', style: TextStyle(color: Colors.white)),
+                secondary: Icon(Icons.directions_walk, color: const Color.fromARGB(255, 255, 80, 80)),
+                onChanged: (value) {
+                  setState(() {
+                    selectedReportType = value;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nota (opcional)',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: const Color.fromARGB(66, 255, 255, 255)),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: const Color.fromARGB(105, 255, 255, 255)),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+                onChanged: (value) {
+                  setState(() {
+                    note = value;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text(
+                      'Cancelar',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: selectedReportType != null
+                        ? () {
+                            widget.onReportTypeSelected(selectedReportType!, note);
+                            Navigator.of(context).pop();
+                          }
+                        : null, // Botón deshabilitado si no hay una selección
+                    child: Text('Subir reporte'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: const Color.fromARGB(255, 255, 255, 255), // Cambia el color si lo deseas
+                      backgroundColor: AppColors.buttonIcon,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text('Cancelar'),
-        ),
-      ],
     );
   }
 }
